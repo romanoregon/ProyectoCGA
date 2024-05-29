cf85bcf01da7bc7f92666a07576e2920bd7da8c4
romanoregon <romanoregon@gmail.com>
2024-05-29 15:38:13 -0600

    Inclusion de sombras y neblina de practica 13

--- Shaders/multipleLights_shadow.fs
+++ Shaders/multipleLights_shadow.fs
@@ -58,7 +58,7 @@ uniform vec3 viewPos;
 uniform sampler2D texture1;
 
 uniform sampler2D shadowMap;
-
+ 
 uniform vec3 fogColor;
 
 float calculateShadow(vec3 lightDir){
@@ -79,15 +79,15 @@ float calculateShadow(vec3 lightDir){
 	// float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
 
 	// This is the third version of the shadow mapping with shadow bias to correct the shadow acne
-	// vec3 normal = normalize(our_normal);
-	// float bias = max(0.005 * (1.0 - dot(normal, lightDir)), 0.001);  
-	// float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+	//vec3 normal = normalize(our_normal);
+	//float bias = max(0.005 * (1.0 - dot(normal, lightDir)), 0.001);  
+	//float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
 
 	// This is the four version of the shadow mapping with shadow bias to correct the shadow acne
-	// vec3 normal = normalize(our_normal);
-	// float bias = max(0.005 * (1.0 - dot(normal, lightDir)), 0.001);  
-	// float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
-	// if(projCoords.z > 1.0)
+	//vec3 normal = normalize(our_normal);
+	//float bias = max(0.005 * (1.0 - dot(normal, lightDir)), 0.001);  
+	//float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+	//if(projCoords.z > 1.0)
     //	shadow = 0.0;
 
 
