0cbbd9de486fef22c55d136b802c5c7d85b115b0
alfonso1091 <watanuki0291@gmail.com>
2024-05-29 22:49:14 -0600

    Personajes / Modelos

---
+++ build/Proyecto/CMakeFiles/Proyecto.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/Users/Alfonso/Documents/Proyecto/ProyectoCGA/bin/Proyecto.exe"
+  "C:/Users/Alfonso/Documents/Proyecto/ProyectoCGA/bin/Proyecto.exe.manifest"
+  "C:/Users/Alfonso/Documents/Proyecto/ProyectoCGA/bin/Proyecto.pdb"
+  "CMakeFiles/Proyecto.dir/src/main.cpp.obj"
+  "CMakeFiles/Proyecto.dir/src/main.cpp.obj.d"
+  "libProyecto.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
